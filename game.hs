044e819908a708e5941a5cee0728f6fddf40ee0b
jcelsocosta <jccl@cin.ufpe.br>
2018-12-11 01:37:40 -0200

    Add files via upload

--- game.hs
+++ game.hs
@@ -1,5 +1,3 @@
-
-
 module Main where
 
 import Graphics.UI.Fungen
@@ -9,7 +7,7 @@ import Control.Concurrent
 import Control.Concurrent.MVar
 import Control.Concurrent.STM
 
-data GameAttribute = Score Int
+data GameAttribute =  Score  Int
 
 numbers = 0
 width = 400
@@ -44,32 +42,32 @@ main = do
 createBall ::  GameObject ()
 createBall =
   let ballPic = Basic (Circle 17.0 0.0 1.0 0.0 Filled)
-  in object "ball" ballPic False (w/2,h-10) (0,-4) ()
+  in object "ball" ballPic False (w/2,h-10) (1,-5) ()
 
 createBall2 :: GameObject ()
 createBall2 =
   let ballPic = Basic (Circle 17.0 0.0 1.0 0.0 Filled)
-  in object "ball2" ballPic False (w-50,h-50) (0,-8) ()
+  in object "ball2" ballPic False (w-50,10000) (0,-8) ()
 
 createBall3 :: GameObject ()
 createBall3 =
   let ballPic = Basic (Circle 17.0 0.0 1.0 0.0 Filled)
-  in object "ball3" ballPic False ((w/2)+60,h-50) (0,-6) ()
+  in object "ball3" ballPic False ((w/2)+60,20000) (0,-6) ()
 
 createBall4 :: GameObject ()
 createBall4 =
   let ballPic = Basic (Circle 17.0 0.0 1.0 0.0 Filled)
-  in object "ball4" ballPic False (50,h-50) (0,-7) ()
+  in object "ball4" ballPic False (50,30000) (0,-7) ()
 
 createBall5 :: GameObject ()
 createBall5 =
   let ballPic = Basic (Circle 17.0 0.0 1.0 0.0 Filled)
-  in object "ball5" ballPic False ((w/2)-50,h-50) (0,-6) ()
+  in object "ball5" ballPic False ((w/2)-50,40000) (0,-6) ()
 
 createBar :: GameObject ()
 createBar =
   let barBound = [(-25,-6),(25,-6),(25,6),(-25,6)]
-      barPic   = Basic (Polyg barBound 1.0 1.0 1.0 Filled)
+      barPic   = Basic (Polyg barBound 2.0 2.0 2.0 Filled)
   in object "bar" barPic False (w/2,30) (0,0) ()
 
 
@@ -128,37 +126,37 @@ gameCycle = do
   
   col4 <- objectBottomMapCollision ball
   when col4  ( do
-    setObjectPosition ((w/2),h-10)   ball
+    setObjectPosition ((w/2),h-20)   ball
     setGameAttribute (Score (n+1))
        
               )
-  when (n>=10) (do
-      setObjectPosition (0,1000) ball
-    )
-  when (n>=20)(do
-      setObjectPosition (0,1000) ball2
-    )
- 
-  when (n>=30) (do
-      setObjectPosition (0,1000) ball3
-    )
-  when (n>=35)(do
-      setObjectPosition (0,1000) ball4
-    )
-  when (n>=40)(do
-      setObjectPosition (0,1000) ball5
 
-    )   
+  when (n<3)(do
+      setObjectPosition ((w-50),h+100) ball2
+              )
+    
+ 
+  when (n<8) (do
+      setObjectPosition ((w/2)+50,h+100) ball3
+            )
+    
+  when (n<10)(do
+      setObjectPosition (50,h+100) ball4
+              )
+    
+  when (n<12)(do
+      setObjectPosition ((w/2)-50,h+100) ball5
+             )   
 
   col6 <- objectBottomMapCollision ball2
   when col6 ( do
-      setObjectPosition ((w-50),h-50) ball2
+      setObjectPosition ((w-50),h-10) ball2
       setGameAttribute (Score (n+1))
 
               )
   col8 <- objectBottomMapCollision ball3
   when col8 (do
-      setObjectPosition ((w/2)+50,h-50) ball3
+      setObjectPosition ((w/2)+50,h) ball3
       setGameAttribute (Score (n+1))
             )
 
